d5d0da6b045324e1c47a46db972933bea5571edd
Pabular, Julius <julius.pabular@gmail.com>
2025-01-23 18:07:05 +0800

    Refactor utility classes to standard final classes.
    Replaced Lombok's `@UtilityClass` with standard `final` classes and added `static` modifiers to relevant methods and fields for consistency. This improves transparency and maintains explicit Java conventions without relying on Lombok.

--- src/main/java/tech/challenge/speech/common/ApiResponseBuilder.java
+++ src/main/java/tech/challenge/speech/common/ApiResponseBuilder.java
@@ -1,21 +1,19 @@
 package tech.challenge.speech.common;
 
-import lombok.experimental.UtilityClass;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import tech.challenge.speech.model.dto.ApiResponseWrapper;
 
 import java.util.List;
 
-@UtilityClass
-public class ApiResponseBuilder {
+public final class ApiResponseBuilder {
 
-    public <T> ResponseEntity<ApiResponseWrapper<T>> buildResponse(HttpStatus status, String message, T data, List<String> errors) {
+    public static <T> ResponseEntity<ApiResponseWrapper<T>> buildResponse(HttpStatus status, String message, T data, List<String> errors) {
         ApiResponseWrapper<T> response = new ApiResponseWrapper<>(status.value(), message, data, errors);
         return ResponseEntity.status(status).body(response);
     }
 
-    public <T> ResponseEntity<ApiResponseWrapper<T>> buildResponse(HttpStatus status, String message, T data) {
+    public static <T> ResponseEntity<ApiResponseWrapper<T>> buildResponse(HttpStatus status, String message, T data) {
         return buildResponse(status, message, data, null);
     }
 }
--- src/main/java/tech/challenge/speech/common/Constants.java
+++ src/main/java/tech/challenge/speech/common/Constants.java
@@ -1,12 +1,10 @@
 package tech.challenge.speech.common;
 
-import lombok.experimental.UtilityClass;
+public final class Constants {
 
-@UtilityClass
-public class Constants {
-    public final String SPEECHES_RETRIEVED = "Speech/es retrieved successfully";
-    public final String SPEECH_CREATED = "Speech created successfully";
-    public final String SPEECH_UPDATED = "Speech updated successfully";
-    public final String SPEECH_DELETED = "Speech deleted successfully";
-    public final String ID_CONFLICT_MESSAGE = "Conflict: ID in path does not match ID in request body";
+    public static final String SPEECHES_RETRIEVED = "Speech/es retrieved successfully";
+    public static String SPEECH_CREATED = "Speech created successfully";
+    public static String SPEECH_UPDATED = "Speech updated successfully";
+    public static String SPEECH_DELETED = "Speech deleted successfully";
+    public static String ID_CONFLICT_MESSAGE = "Conflict: ID in path does not match ID in request body";
 }
